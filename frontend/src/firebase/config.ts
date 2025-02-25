0e410927e9d63198b9f77b7a80489c5a9b31e2f2
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2025-02-25 02:04:32 -0800

    removed the firebase key

--- frontend/src/firebase/config.ts
+++ frontend/src/firebase/config.ts
@@ -6,15 +6,7 @@ import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD4hHhGzF6GNaVu4Eia2mdbtOUw_1bJOgo",
-  authDomain: "stocker-db315.firebaseapp.com",
-  projectId: "stocker-db315",
-  storageBucket: "stocker-db315.firebasestorage.app",
-  messagingSenderId: "620590333368",
-  appId: "1:620590333368:web:e942bb5ecd35788911234d",
-  measurementId: "G-X981MEJ8Q2"
-};
+
 
 // Initialize Firebase
 console.log("Initializing Firebase with config:", { ...firebaseConfig, apiKey: "HIDDEN" });
